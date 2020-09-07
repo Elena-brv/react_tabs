6398a2bab56c7d779d486349f5959dd09d75a3a5
Elena-brv <leena.brv@gmail.com>
2020-09-07 17:32:29 +0300

    Updates

---
+++ precache-manifest.4cd8d4475a536e794db354accdc14c92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9952e67d4ff9e0768f9fc14959becc9d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d69a75a0ac846d405c2e",
+    "url": "./static/css/main.b5a02842.chunk.css"
+  },
+  {
+    "revision": "b19d6e15778bba63ab0b",
+    "url": "./static/js/2.c3c4153a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.c3c4153a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69a75a0ac846d405c2e",
+    "url": "./static/js/main.fec75cfd.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  }
+]);
